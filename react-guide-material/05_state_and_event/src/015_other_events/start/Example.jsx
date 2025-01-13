a53718bba834426df0926146d2a519728437c8ac
Masa 7134 <masa@mba.local>
2025-01-13 18:53:04 +0900

    その他のイベントハンドラー

--- react-guide-material/05_state_and_event/src/015_other_events/start/Example.jsx
+++ react-guide-material/05_state_and_event/src/015_other_events/start/Example.jsx
@@ -8,8 +8,11 @@ const Example = () => {
         入力値のイベント：
         <input
           type="text"
+          // 入力すると出力される
           onChange={() => console.log("onChange検知")}
+          // フォーカスが外れると出力
           onBlur={() => console.log("onBlur検知")}
+          // フォーカスされると出力
           onFocus={() => console.log("onFocus検知")}
         />
       </label>
