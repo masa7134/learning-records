be9b92364907aca5ca1477ae58b0ecfad755e7e5
Masa 7134 <masa@mba.local>
2025-01-23 22:11:36 +0900

    オブジェクトの注意点

--- react-guide-material/05_state_and_event/src/060_state_object/start/Example.jsx
+++ react-guide-material/05_state_and_event/src/060_state_object/start/Example.jsx
@@ -6,10 +6,21 @@ import { useState } from "react";
 const Example = () => {
   const personObj = { name: "Tom", age: 18 };
   const [person, setPerson] = useState(personObj);
+
   const changeName = (e) => {
+    person.name = '';
+      console.log({ ...person } === person);
+      console.log({ ...person, name: e.target.value });//　こちらは入力されて表示される内容と同じになる
     // setPersonはオブジェクトで渡す必要がある。名前だけでnullで上書きされて空欄表示になってしまう
-    setPerson({name: e.target.value, age: person.age})
-  };
+  //   setPerson({name: e.target.value, age: person.age})
+  // };
+
+  // 下記のようにスプレッド演算子をしようすると、すべてのプロパティを記述しなくても、すべて上書きされて新しいオブジェクトが生成したことになる
+  //　セットパーソンにわたすオブジェクトは８行目のpersonのオブジェクトと必ず違うオブジェクトでなくてはならない
+      setPerson({ ...person, name: e.target.value })
+  }
+
+
   // eはイベントハンドラーのこと。イベントハンドラーはイベントに対する関数のこと
   const changeAge = (e) => {
   //event.targetとはイベントが発生した要素そのもの、.valueとはその要素の現在の値（ユーザーが入力したテキストや選択した値など）
