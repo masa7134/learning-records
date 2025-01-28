f33ff2233dd5933df094c91c86693ab4ca025d72
Masa 7134 <masa@mba.local>
2025-01-28 11:07:34 +0900

    ステートとプロップすについて

--- react-guide-material/05_state_and_event/src/080_state_and_props/start/Example.jsx
+++ react-guide-material/05_state_and_event/src/080_state_and_props/start/Example.jsx
@@ -1,19 +1,27 @@
 import { useState } from "react";
 
+// 親コンポーネント
 const Example = () => {
   const [ toggle, setToggle ] = useState(true);
+  const [countA, setCountA] = useState(0);
+  const [countB, setCountB] = useState(0);
   const toggleComponent = () => {
     setToggle(prev => !prev);
   }
   return (
     <>
     <button onClick={toggleComponent}>toggle</button>
-    {toggle ? <Count key="A" title="A"/> : <Count key="B" title="B"/>}
+    
+     {/* // プロップス（コンポーネント間でやり取りされるデータ） */}
+     {toggle 
+     ? <Count key="A" title="A" count={countA} setCount={setCountA}/>
+     : <Count key="B" title="B" count={countB} setCount={setCountB}/>}
     </>
   )
 }
-const Count = ({ title }) => {
-  const [count, setCount] = useState(0);
+// 子コンポーネント部分　、 引数でpropsを受け取る
+const Count = ({ title, count, setCount }) => {
+  // const [count, setCount] = useState(0);  1,このuseStateを親コンポーネントに移動
   const countUp = () => {
     setCount((prevstate) => prevstate + 1);
   };
