a958e4ff35ab71537451a79e3d3cbdd652e7f051
Masa 7134 <masa@mba.local>
2025-02-01 10:20:57 +0900

    条件分岐

--- react-guide-material/06_control_and_form/src/060_conditional_render/start/Example.jsx
+++ react-guide-material/06_control_and_form/src/060_conditional_render/start/Example.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const Example = () => {
-  const animals = ["Dog", "Cat", "Rat"];
+  const animals = ["Dog", "Cat", null, "Rat"];
 
   const [filterVal, setFilterVal] = useState("");
 
@@ -15,13 +15,31 @@ const Example = () => {
       <ul>
         {animals
           .filter((animal) => {
-            const isMatch = animal.indexOf(filterVal) !== -1;
-            console.log(animal.indexOf(filterVal));
+            const animalStr = animal ?? ""; // Null合体演算子
+            const isMatch = animalStr.indexOf(filterVal) !== -1;
             return isMatch;
           })
-          .map((animal) => (
-            <li key={animal}>{animal}</li>
-          ))}
+          .map((animal) => {
+
+            // if文
+            // if(animal === "Dog") {
+            //   return <li key={animal}>{animal} + ★</li>
+            // } else {
+            //   return <li key={animal}>{animal}</li>
+            // }
+
+            return <li key={animal}>{
+            // 三項演算、重複する記述がすくないので良いコードといえる、上のif分だとreturn <li>などが重複している
+              // animal + (animal === "Dog" 
+              // ? "★"
+              // : "")}
+              // &&  (アンド条件)さらにコードを少なくできる,null合体演算子も利用
+              animal ?? "nullまたはundefinedでした"}{ animal === "Dog" &&  "★" }
+            </li>
+
+
+
+          })}
       </ul>
     </>
   );
