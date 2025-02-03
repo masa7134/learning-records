aae89e9af059b94871600f9ccfcc3bc197537018
Masa 7134 <masa@mba.local>
2025-02-03 09:53:30 +0900

    inputとtextareaの作成方法

--- react-guide-material/06_control_and_form/src/080_input_textarea/start/Example.jsx
+++ react-guide-material/06_control_and_form/src/080_input_textarea/start/Example.jsx
@@ -1,10 +1,29 @@
+import { useState } from "react";
+
 const Example = () => {
+  const [val, setValue] = useState();
+  const clearVal = () => setValue("");
+  
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
+    <div>
+      <label htmlFor="123">ラベル</label>
+      <div>
+        <input 
+          id="123" 
+          placeholder="こんにちは"
+          value={val}
+          onChange={(e) => setValue(e.target.value)}
+          />
+        <textarea
+          id="456" 
+          placeholder="こんにちは"
+          value={val}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <h3>{val}</h3>
+        <button onClick={clearVal}>クリア</button>
+      </div>
+    </div>
   );
 };
 
