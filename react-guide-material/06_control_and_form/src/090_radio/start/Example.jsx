b8ffc746c9ab0f574a17ff9ab91a19d296b96663
Masa 7134 <masa@mba.local>
2025-02-03 10:28:01 +0900

    radioボタン作成

--- react-guide-material/06_control_and_form/src/090_radio/start/Example.jsx
+++ react-guide-material/06_control_and_form/src/090_radio/start/Example.jsx
@@ -1,10 +1,56 @@
+import { useState } from "react";
+
 const Example = () => {
+  const [fruit, setFruit] = useState("Apple");
+  const onChange = (e) => setFruit(e.target.value);
+
+  // map用
+  const RADIO_COLLECTION = ["Apple", "Banana", "Cherry"];
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
+    <>
+      {RADIO_COLLECTION.map((value) => {
+
+        return (
+          <label key={value}>
+            <input type="radio"
+              value={value} // 選択されたときに取得したい値
+              checked={fruit === value} // trueのときにラジオボタンにチェックがつく
+              onChange={onChange}
+            />
+            {value}
+          </label>
+        );
+      })}
+
+
+
+
+      {/* <label>
+        <input type="radio"
+          value="Apple" // 選択されたときに取得したい値
+          checked={fruit === "Apple"} // trueのときにラジオボタンにチェックがつく
+          onChange={onChange}
+        />
+        Apple
+      </label>
+      <label>
+        <input type="radio"
+          value="Banana" // 選択されたときに取得したい値
+          checked={fruit === "Banana"} // trueのときにラジオボタンにチェックがつく
+          onChange={onChange}
+        />
+        Banana
+      </label>
+      <label>
+        <input type="radio"
+          value="Cherry" // 選択されたときに取得したい値
+          checked={fruit === "Cherry"} // trueのときにラジオボタンにチェックがつく
+          onChange={onChange}
+        />
+        Cherry
+      </label> */}
+      <h3>私は{fruit}が食べたい</h3>
+    </>
   );
 };
 
