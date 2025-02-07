40ed0b231d3fb463ad4602ae44b7422feb29b24a
Masa 7134 <masa@mba.local>
2025-02-07 10:03:36 +0900

    インラインスタイルについて

--- react-guide-material/07_styling_component/src/010_inline_style/start/Example.jsx
+++ react-guide-material/07_styling_component/src/010_inline_style/start/Example.jsx
@@ -5,10 +5,24 @@ const Example = () => {
 
     const clickHandler = () => setIsSelected(prev => !prev);
 
+    const style = { // ボタンのスタイル
+        width: 120,
+        height: 60,
+        display: "block", // ""を忘れずに！
+        fontWeight: "bold", // font-weightのようなハイフンスタイルではないので注意
+        "border-radius": 9999, // ハイフンスタイルで書きたいときは""で囲う
+        cursor: "pointer",
+        border: "none",
+        margin: "auto",
+        background: isSelected ? "pink" : "",
+    }
+
     return (
         <>
-            <button onClick={clickHandler}>ボタン</button>
-            <div>{isSelected && "クリックされました。"}</div>
+            <button onClick={clickHandler} style={style}>ボタン</button>
+            {/* メッセージのスタイルは直接書き込み */}
+            {/* 二重波括弧使用　{{}} 外側：jsx, 内側：オブジェクト */}
+            <div style={{ textAlign:"   "}}>{isSelected && "クリックされました。"}</div>
         </>
     )
 };
