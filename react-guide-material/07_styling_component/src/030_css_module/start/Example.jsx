483162ca6ab5519b727b98035eea209c3bc37343
Masa 7134 <masa@mba.local>
2025-02-08 10:15:15 +0900

    cssモジュールについて

--- react-guide-material/07_styling_component/src/030_css_module/start/Example.jsx
+++ react-guide-material/07_styling_component/src/030_css_module/start/Example.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 
 import SubButton from "./components/SubButton";
-import "./Example.css";
+import style from "./Example.module.css";
+
+// console.log(style);
 
 const Example = () => {
   const [isSelected, setIsSelected] = useState(false);
@@ -10,7 +12,7 @@ const Example = () => {
 
   return (
     <>
-      <button className={`btn ${isSelected ? "selected" : ""}`} onClick={clickHandler}>
+      <button className={`${style.btn} ${isSelected ? style.selected : ""}`} onClick={clickHandler}>
         ボタン
       </button>
       <SubButton />
