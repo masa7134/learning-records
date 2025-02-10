cae12f3e69a735113a5039caa18bd6b2316d747a
Masa 7134 <masa@mba.local>
2025-02-10 12:21:02 +0900

    chakra使用方法

--- react-guide-material/07_styling_component/src/050_chakra_ui/start/components/Form.jsx
+++ react-guide-material/07_styling_component/src/050_chakra_ui/start/components/Form.jsx
@@ -1,9 +1,22 @@
+import { Button, HStack, Input, useToast } from "@chakra-ui/react";
 import { useState } from "react";
 const Form = ({ createTodo }) => {
   const [enteredTodo, setEnteredTodo] = useState("");
 
+  const toast = useToast();
+
   const addTodo = (e) => {
     e.preventDefault();
+
+    if (!enteredTodo) {
+      toast({
+        title: "新しいタスクを入力してください",
+        status: "error",
+        duration: 2000,
+        isClosable: true,
+      });
+      return; 
+    }
 
     const newTodo = {
       id: Math.floor(Math.random() * 1e5),
@@ -15,16 +28,30 @@ const Form = ({ createTodo }) => {
     setEnteredTodo("");
   };
   return (
-    <div>
-      <form onSubmit={addTodo}>
-        <input
+    <form onSubmit={addTodo}>
+      <HStack>
+        <Input
+          placeholder="新しいタスク"
+          _placeholder={{ opacity: "0.3", color: "gray.500" }}
+          size="lg"
+          padding="3"
+          bgColor="white"
           type="text"
           value={enteredTodo}
           onChange={(e) => setEnteredTodo(e.target.value)}
         />
-        <button>追加</button>
-      </form>
-    </div>
+        <Button
+          colorScheme="blue"
+          size="md"
+          bgColor="white"
+          variant="outline"
+          px={7}
+          type="submit"
+        >
+          追加
+        </Button>
+      </HStack>
+    </form>
   );
 };
 
