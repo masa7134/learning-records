1497cb2873364d31bbd9b5dd12f878b84648e15a
Masa 7134 <masa@mba.local>
2025-02-18 11:19:48 +0900

    イミュータビリティ説明

--- react-guide-material/10_functional_programming/src/030_immutability/start/Example.jsx
+++ react-guide-material/10_functional_programming/src/030_immutability/start/Example.jsx
@@ -1,4 +1,29 @@
 const Example = () => {
+  // 関数型（純粋関数）
+  // ・fn(決まった引数) -> 決まった戻り値
+  // ・関数外の状態（データ）は参照・変更しない。
+  // ・関数外に影響を及ぼさない。
+  // POINT 引数で渡された値を変更しない。（Immutabilityの保持）
+  // 上記の要件を満たさない操作は「副作用」と呼ぶ。
+
+  const num = { val: 2 }
+
+  // 純粋関数ではない
+  // const double = () => {
+  //   num.val = num.val * 2;
+  //   return num;
+  // }
+
+  // immutabilityが保持された状態
+  const double = () => {
+    const newNum = {};
+    newNum.val = num.val * 2;
+    return newNum;
+  }
+
+  const newNum = double(num);
+  console.log('newNum', newNum, 'num', num)
+
   return (
     <>
       <h3>不変性（Immutability）</h3>
