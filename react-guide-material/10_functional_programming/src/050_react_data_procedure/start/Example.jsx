143eab8279147ca41315dc5e70fb027122663c81
Masa 7134 <masa@mba.local>
2025-02-19 09:45:54 +0900

    関数コンポーネントのルール

--- react-guide-material/10_functional_programming/src/050_react_data_procedure/start/Example.jsx
+++ react-guide-material/10_functional_programming/src/050_react_data_procedure/start/Example.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 
-const Example = () => {
+
+// stateは状態管理と処理を分離している
+const Example = () => { // 関数コンポーネントはJSXを作成する場所
   const [ state, setState ] = useState(0);
   const increment = () => {
     setState(state + 1);
   }
+  // window.alert('hello') 関数外に影響を及ぼす記述なので不可,副作用
   return (
     <>
       <button onClick={increment}>+</button>
