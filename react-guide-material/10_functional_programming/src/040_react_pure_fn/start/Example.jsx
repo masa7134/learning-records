ba762d193079f028830c421ba642613ac72a8497
Masa 7134 <masa@mba.local>
2025-02-19 09:29:36 +0900

    Reactの関数ルール

--- react-guide-material/10_functional_programming/src/040_react_pure_fn/start/Example.jsx
+++ react-guide-material/10_functional_programming/src/040_react_pure_fn/start/Example.jsx
@@ -1,7 +1,25 @@
+let value = 0;// ダメな例 関数のスコープ外で定義しているため
+
+const Child = () => { //ダメな関数
+  value++;
+  return <div>{value}</div>
+}
+
+const ChildPure = ({ value }) => {
+  return <div>{value}</div>
+}
+
 const Example = () => {
+  let value = 0;
 
   return (
     <>
+      <Child/>
+      <Child/>
+      <Child/>
+      <ChildPure value={++value}/>
+      <ChildPure value={++value}/>
+      <ChildPure value={++value}/>
     </>
   );
 };
